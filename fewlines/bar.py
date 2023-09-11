12b97d7b934cedd135f425fd665a8dbcb44e62d9
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2023-09-11 09:47:46 -0400

    fewlines: comments, red color scheme

--- fewlines/bar.py
+++ fewlines/bar.py
@@ -1,10 +1,17 @@
 import math
 
+# Blocks to use to plot charts.
+
 # not using the largest block so that two histograms on two lines won't collide
 bar_blocks     = [' ', '▁', '▂', '▃', '▄', '▅', '▆', '▇']
+
+# For horizon we can use the largest block, as we'll use color coding
 horizon_blocks = [' ', '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█']
+
+# colorschemes
 colors = {
-    'green': [-1, 150, 107, 22]
+    'green': [-1, 150, 107, 22],
+    'red'  : [-1, 196, 124, 52],
 }
 
 def histogram(data, bins, scale_range=None, ignore_outliers=False):
@@ -33,7 +40,7 @@ def histogram(data, bins, scale_range=None, ignore_outliers=False):
     
     return bin_counts, bin_edges
 
-def top_axis_str(mn, mx, chart_width=60, left_margin=20):
+def top_axis_str(mn, mx, chart_width, left_margin):
     mn_text, mx_text = f' {mn:.3g}|'[-left_margin:], f'{mx:.3g}'
     if left_margin <= 0:
         return '_' * chart_width + f'|{mx_text}'
@@ -53,7 +60,7 @@ def global_range(numbers):
     mx = global_stat(numbers, max)
 
     if mn is None or mx is None:
-        mn, mx = 0, 0
+        mn, mx = 0.0, 0.0
 
     if mn == mx:
         mx += 1
@@ -61,8 +68,7 @@ def global_range(numbers):
     
     return mn, mx
 
-# this just plots a line using blocks. 
-# more suitable for log files
+# bar_line plots a line using blocks without color coding. more suitable for log files
 def bar_line(y, chr=bar_blocks) -> str:
     Y = max(y)
     if Y == 0:
@@ -72,7 +78,7 @@ def bar_line(y, chr=bar_blocks) -> str:
     horizon = ''.join([cell(v) for v in y])
     return horizon
 
-# this plots line using block characters and color - suitable for terminals
+# horizon_line plots line using blocks and color - suitable for terminal output
 def horizon_line(y, color='green', chrs=horizon_blocks) -> str:
     bg = [f'\33[48;5;{c}m' if c >= 0 else '' for c in colors[color]]
     fg = [f'\33[38;5;{c}m' if c >= 0 else '' for c in colors[color]]
@@ -88,6 +94,12 @@ def horizon_line(y, color='green', chrs=horizon_blocks) -> str:
 
 ### Actual API
 
+# Plot multiple histograms on the same scale.
+#   numbers     - a dictionary{str: list_of_numbers} of data to plot distribution on
+#   chart_width - how many characters to use. Histogram will use that many bins.
+#   axis        - show a line with range at the top
+#   left_margin - width of the space for each data title
+#   color       - name of the colorscheme. If None, uses blocks only.
 def bar_histograms(numbers, chart_width=60, axis=True, left_margin=20, color=None):
     mn, mx = global_range(numbers)
     res = []
@@ -110,16 +122,25 @@ def bar_histograms(numbers, chart_width=60, axis=True, left_margin=20, color=Non
 
     return res
 
-def bar_histogram(values, title='', chart_width=80, axis=True, left_margin=20, color=None):
+# Plot single histogram
+#   values      - a list of numbers to plot distribution on
+#   title       - name of the data
+#   chart_width - how many characters to use. Histogram will use that many bins.
+#   axis        - show a line with range at the top
+#   left_margin - width of the space for each data title
+#   color       - name of the colorscheme. If None, uses blocks only.
+def bar_histogram(values, title='', chart_width=60, axis=True, left_margin=20, color=None):
     return bar_histograms({title: values}, chart_width, axis, left_margin, color)
 
+# Some test example, requires numpy
 if __name__ == '__main__':
     import numpy as np
     data = {'title_A': list(np.random.normal(size=10000))}
-
+    
     # horizon with colors
-    for l in bar_histograms(data, chart_width=40, color='green'):
-        print(l)
+    for color in colors:
+        for l in bar_histograms(data, chart_width=40, color=color):
+            print(l)
 
     # bar chart without colors
     for l in bar_histograms(data, chart_width=40):
