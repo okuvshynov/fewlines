a669d51f73797e5436cbbf8bc9a6e7efc4852e09
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-04-08 11:24:47 -0400

    bump version

--- py/setup.py
+++ py/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fewlines',
-    version='0.0.9',
+    version='0.1.0',
     author='Oleksandr Kuvshynov',
     author_email='okuvshynov@gmail.com',
     description='Bar charts and horizon chart in terminal/log files.',
